065550b71b55ef77a202c68131d0f305375b52d5
Alfred Christian Emmanuel Ngaosi <2232837@slu.edu.ph>
2024-11-29 13:22:29 +0800

    added initial placeholder files for manager

---
+++ frontend/src/manager/pages/ManagerHub.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+const React = () => {
+
+    return(
+        <>
+        <h1>Manager's Hub</h1>
+        <h2>Statistcs</h2>
+        <div></div>
+        <h2>Applications</h2>
+        <div></div>
+        </>
+       
+    );
+
+};
