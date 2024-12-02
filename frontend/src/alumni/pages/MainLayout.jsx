4fff2091eacea6a00fc06a2200d456dd3699e78c
Giovanni Leo <2232254@slu.edu.ph>
2024-12-02 18:52:04 +0800

    socket io setup

--- frontend/src/alumni/pages/MainLayout.jsx
+++ frontend/src/alumni/pages/MainLayout.jsx
@@ -1,17 +1,20 @@
 import React from "react";
 import Navbar from "../components/Navbar";
 import { Outlet } from "react-router-dom";
+import SocketProvider from "../context/SocketContext";
 
 const MainLayout = () => {
   return (
-    <main>
-      <header className="padding-x fixed w-full top-0 z-50">
-        <Navbar />
-      </header>
-      <section className="padding mt-11">
-        <Outlet />
-      </section>
-    </main>
+    <SocketProvider>
+      <main>
+        <header className="padding-x fixed w-full top-0 z-50">
+          <Navbar />
+        </header>
+        <section className="padding mt-11">
+          <Outlet />
+        </section>
+      </main>
+    </SocketProvider>
   );
 };
 
--- server/index.js
+++ server/index.js
@@ -1,6 +1,8 @@
 import express from "express";
 import cookieParser from "cookie-parser";
 import cors from "cors";
+import { createServer } from "http";
+import { Server as SocketIO } from "socket.io";
 import "dotenv/config";
 
 import { router as registerRoute } from "./routes/register.js";
@@ -10,24 +12,75 @@ import { programRouter } from "./routes/programs.js";
 import { applicationRoute } from "./routes/application.js";
 
 const app = express();
+const httpServer = createServer(app);
 
+/**
+ * ======================
+ *    MIDDLEWARES
+ * =======================
+ */
 app.use(
   cors({
     origin: process.env.ENDPOINT,
     credentials: true,
   })
 );
-
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 
+/**
+ * ======================
+ *   ROUTES
+ * =======================
+ */
 app.use("/api", usersRoute);
 app.use("/api/register", registerRoute);
 app.use("/api", postRouter);
 app.use("/api", programRouter);
 app.use("/api", applicationRoute);
 
+/**
+ * ======================
+ *   SOCKET IO
+ * =======================
+ */
+
+const users = new Map();
+
+const addUser = (userId, socketId) => {
+  users.set(userId, socketId);
+};
+
+const removeUser = (userId) => {
+  users.delete(userId);
+};
+
+const io = new SocketIO(httpServer, {
+  cors: {
+    origin: process.env.ENDPOINT,
+  },
+});
+
+io.on("connection", (socket) => {
+  socket.on("register", (userId) => {
+    addUser(userId, socket.id);
+    console.log(`A user connected: userId: ${userId} socketId: ${socket.id}`);
+  });
+
+  socket.on("unregister", (userId) => {
+    removeUser(userId);
+    console.log(`User disconnected: userId: ${userId} socketId: ${socket.id}`);
+  });
+});
+
+/**
+ * ======================
+ *   STARTING THE SERVER
+ * =======================
+ */
 const PORT = process.env.PORT || 1099;
 
-app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
+httpServer.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
