d0363984338d3f9c12f783e4fcac12f3cdb886f9
Jhea Jhana Prudencio <2232799@slu.edu.ph>
2024-12-10 13:11:32 +0800

    modified joblisting placeholder (initial)

--- frontend/src/alumni/components/createPost/Joblisting.jsx
+++ frontend/src/alumni/components/createPost/Joblisting.jsx
@@ -1,7 +1,14 @@
 import React from "react";
 
+import company from "../../../assets/company.png";
+import salary from "../../../assets/money.png";
+import worktype from "../../../assets/worktype.png";
+import experience from "../../../assets/experience.png";
+
 const Joblisting = () => {
-  return <div>Joblisting</div>;
+  return <div>Add Company
+    <img src={company} alt="company"/>
+  </div>;
 };
 
 export default Joblisting;
