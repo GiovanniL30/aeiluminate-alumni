f79884ba165b7eef3b485185d43689f25481ead6
Eugene Kyle C. Patano <2233071@slu.edu.ph>
2024-12-10 12:53:56 +0800

    Added Event fields in EventInformation.jsx

--- frontend/src/alumni/components/createPost/EventInformation.jsx
+++ frontend/src/alumni/components/createPost/EventInformation.jsx
@@ -1,7 +1,22 @@
 import React from "react";
 
 const EventInformation = () => {
-  return <div>Event</div>;
+  return (
+  <div>
+    <div>
+      <img src="" alt="" />
+      Add Location
+    </div>
+    <div>
+      <img src="" alt="" />
+      Category
+    </div>
+    <div>
+      <img src="" alt="" />
+      Date and Time
+    </div>
+  </div>
+  );
 };
 
 export default EventInformation;
