ca93a09ceb36be11845ef7be74973317bc8be3da
Jhea Jhana Prudencio <2232799@slu.edu.ph>
2024-12-10 12:49:42 +0800

    Revert "added icons in joblistings"
    This reverts commit 118ba06f5d60eda2c3f03a207a0a36f08828de03.

--- frontend/src/alumni/components/createPost/Joblisting.jsx
+++ frontend/src/alumni/components/createPost/Joblisting.jsx
@@ -1,10 +1,5 @@
 import React from "react";
 
-import company from "../../../assets/Company.png";
-import experience from "../../../assets/experience.png";
-import salary from "../../../assets/money.png";
-import worktype from "../../../assets/worktype.png";
-
 const Joblisting = () => {
   return <div>Joblisting</div>;
 };
